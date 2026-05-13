bd8d6fffca17a02b3db9852b2dc627fe7bc090e7
prakash samanta <itsmepraksh.work@gmail.com>
2026-05-13 23:46:30 +0530

    feat : routing tracker page, create basic form for api data , creation of tracker

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
-  const onClickBtn = ()=>{
-    console.log("its working")
-    return 
-  }
+  const router = useRouter()
+
+  const onClickBtn = ()=> router.push('/tracker') ;
+  
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
          <h1>Create a habit tracker</h1>
 
-         <button onClick={()=>onClickBtn} className="bg-yellow-500 text-black p-2">Create now</button>
+         <button onClick={()=>onClickBtn()} className="bg-yellow-500 text-black p-2">Create now</button>
       </main>
     </div>
   );
--- frontend/app/tracker/page.tsx
+++ frontend/app/tracker/page.tsx
@@ -1,8 +1,33 @@
-
+"use client"
 
 const page = () => {
   return (
-    <div>hii kanhaji deklo</div>
+    <>
+      <div>
+        <h1>fill the data about tracker creation</h1>
+        <form>
+          <label htmlFor="trackerName">tracker name</label>
+          <input id="trackerName" type="text" placeholder="eg. daily.." />
+
+          <label htmlFor="trackerType">tracker type</label>
+          <select name="" id="trackerType">
+            <option >--select--</option>
+            <option value="professional">professional</option>
+            <option value="educational">educational</option>
+            <option value="personal">personal</option>
+          </select>
+
+          <label htmlFor="taskLimit">No. of task</label>
+          <input type="number" placeholder="eg. 5" />
+
+          <label htmlFor="timeLimit">No. of days</label>
+          <input type="number" placeholder="eg, 21" />
+
+          <button>submit</button>
+        </form>
+
+      </div>
+    </>
   )
 }
 
